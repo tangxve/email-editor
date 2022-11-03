2ee2716764b741c518a617d3157bdd58810073c3
tangxve <11183504@qq.com>
2022-11-03 10:14:38 +0800

    feat: init

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import 'vfonts/Lato.css'
+import 'vfonts/FiraCode.css'
+
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+import './assets/style/main.css'
 
 const app = createApp(App)
 
--- src/views/EditorView.vue
+++ src/views/EditorView.vue
@@ -3,5 +3,35 @@
 </script>
 
 <template>
-  2222
+  <n-layout>
+    <n-layout-header>header</n-layout-header>
+    <n-layout has-sider>
+      <n-layout-sider content-style="padding: 24px;">
+        物料
+      </n-layout-sider>
+      <n-layout-content content-style="padding: 24px;">
+        展示
+      </n-layout-content>
+      <n-layout-sider content-style="padding: 24px;">
+        编辑
+      </n-layout-sider>
+    </n-layout>
+  </n-layout>
+  <div class="h-4" />
 </template>
+
+<style scoped>
+.n-layout-header,
+.n-layout-footer {
+  background: rgba(128, 128, 128, 0.2);
+  padding: 24px;
+}
+
+.n-layout-sider {
+  background: rgba(128, 128, 128, 0.3);
+}
+
+.n-layout-content {
+  background: rgba(128, 128, 128, 0.4);
+}
+</style>
